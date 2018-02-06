ddfd2b47eaa171dee27cc4a17352e373a09d15b0
Andrea(Shiny)Voscilla <andrea.voscilla@hotmail.it>
2018-02-06 19:08:02 +0100

    modifica a menu manager e agigunta scena

--- dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/MenuManager.cs
+++ dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/MenuManager.cs
@@ -5,13 +5,26 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
+    bool paused = false;
 
-   
+   public void Pause()
+   {
+        if (!paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+        }
+        else if (paused)
+        {
+            paused =  false;
+            Time.timeScale = 1;
+        }
+   }
 
 
     public void NewGame()
 	{
-		SceneManager.LoadScene ("Main");
+		SceneManager.LoadScene (1);
 	}
 
     public void Exit()
@@ -19,5 +32,9 @@ public class MenuManager : MonoBehaviour {
         Application.Quit();
     }
 
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
    
 }
