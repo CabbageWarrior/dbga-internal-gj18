26a2f540f4c54209e7717818617702d79792a3de
CabbageWarrior <>
2018-02-06 17:56:28 +0100

    Aggiunto conteggio FPS.

--- dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/UIDebugInfos.cs
+++ dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/UIDebugInfos.cs
@@ -7,13 +7,18 @@ public class UIDebugInfos : MonoBehaviour {
 
 	public Text FPSText;
 
-	// Use this for initialization
-	void Start () {
-		
+	float deltaTime = 0.0f;
+
+	void Update()
+	{
+		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//FPSText.text = 
+
+	void OnGUI()
+	{
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		FPSText.text = text;
 	}
 }
