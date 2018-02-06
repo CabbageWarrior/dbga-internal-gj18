21c21a8ff456cd288c967f41ab700605e1543be1
CabbageWarrior <>
2018-02-06 23:22:05 +0100

    Modificate cose nella scena Main e qualche prefab.

---
+++ dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/CondannatoHeadTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CondannatoHeadTrigger : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "Cube") {
+			GetComponent<Rigidbody> ().AddForce ((Camera.main.transform.position - transform.position).normalized * 4, ForceMode.Impulse);
+		}
+	}
+}
