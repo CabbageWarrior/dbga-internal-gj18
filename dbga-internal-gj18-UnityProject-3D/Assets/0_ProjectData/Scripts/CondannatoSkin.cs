e2660547e3bb01dfe30bce60f574b0cb2f3eb5c3
CabbageWarrior <>
2018-02-06 21:59:44 +0100

    Aggiunto Orco Boya.

---
+++ dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/CondannatoSkin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CondannatoSkin : MonoBehaviour {
+	[Header("Prefabs da spawnare")]
+	public GameObject capelliPrefab;
+	public GameObject baffiPrefab;
+
+	[Header("Spawn Points delle personalizzazioni")]
+	public GameObject capelliSpawnPoint;
+	public GameObject baffiSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		if (capelliSpawnPoint.transform.childCount == 0) {
+			//Instantiate (capelliPrefab, capelliSpawnPoint);
+		}
+		if (baffiSpawnPoint.transform.childCount == 0) {
+			//Instantiate (baffiPrefab, baffiSpawnPoint);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
