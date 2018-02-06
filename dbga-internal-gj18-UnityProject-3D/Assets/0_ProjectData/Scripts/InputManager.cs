e4462d247847fcdcd8984b54b9c7f33f1ecca536
dbga_51 <dbga_51@dbga.local>
2018-02-06 14:56:01 +0100

    Input Manager

---
+++ dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+	
+	private GameObject victim;
+	private GameObject Human;
+	private GameObject Human1;
+
+	void Start()
+	{
+		Human = GameObject.Find ("Human");
+		Human1 = GameObject.Find ("Human1");
+	}
+
+	void Update(){
+		
+		if (Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit))
+			{
+				if (hit.transform.name == "Human") {
+					Debug.Log ("This is a Human");
+					victim = Human;
+
+				} else if (hit.transform.name == "Human1") {
+					Debug.Log ("This is a Human1");   
+					victim = Human1;
+
+				} else if (hit.transform.name == "Ceppo" && victim != null) {
+					Debug.Log ( victim + " sgozzato" );   
+					victim = Human1;
+
+				}else {
+					
+					Debug.Log ("This isn't a Player"); 
+				}
+
+			}
+		}
+	}
+}
