e1f0bfea7eb19ed18b9d27dd099bdb61756f2480
CabbageWarrior <>
2018-02-07 18:42:35 +0100

    Messo a posto il boia.

--- dbga-internal-gj18-UnityProject-3D/Assets/PosizionatoBehaviour.cs
+++ dbga-internal-gj18-UnityProject-3D/Assets/PosizionatoBehaviour.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PosizionatoBehaviour : StateMachineBehaviour {
-//	private MonoBehaviour monoBehaviour = null;
-//	public StateMachineBehaviour(MonoBehaviour monoBehaviour){
-//		this.monoBehaviour = monoBehaviour;
-//	}
-
-	public float deltaTime = .2f;
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		//MonoBehaviour StartCoroutine(
 	}
@@ -20,9 +13,9 @@ public class PosizionatoBehaviour : StateMachineBehaviour {
 	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		GameObject.Find ("Boia").GetComponent<Animator> ().SetTrigger ("Kill");
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
@@ -33,10 +26,4 @@ public class PosizionatoBehaviour : StateMachineBehaviour {
 	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
 	//}
-
-	IEnumerator OnStateEnter_Coroutine(){
-		yield return new WaitForSeconds (deltaTime);
-
-		GameObject.Find ("Boia").GetComponent<Animator> ().SetTrigger ("Kill");
-	}
 }
