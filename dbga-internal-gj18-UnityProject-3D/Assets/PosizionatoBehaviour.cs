8204240cfa20893cd591adba9bc0eba12deb30c6
CabbageWarrior <>
2018-02-07 17:42:54 +0100

    Corretta minchiata in uno script.

--- dbga-internal-gj18-UnityProject-3D/Assets/PosizionatoBehaviour.cs
+++ dbga-internal-gj18-UnityProject-3D/Assets/PosizionatoBehaviour.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PosizionatoBehaviour : StateMachineBehaviour {
-
+	float deltaTime = 1f;
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
