2f71d6c22a38bf17c315bf8f8a84a9893dd1966a
dbga_51 <dbga_51@dbga.local>
2018-02-07 15:30:55 +0100

    Audio

---
+++ dbga-internal-gj18-UnityProject-3D/Assets/0_ProjectData/Scripts/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound[] sounds;
+
+	public static AudioManager instance;
+
+	void Awake () {
+
+
+
+		if (instance == null) {
+			instance = this;
+		} 
+		else 
+		{
+			Destroy (gameObject);
+			return;
+		
+		}
+				
+		DontDestroyOnLoad (gameObject);
+
+		sounds = new Sound[transform.childCount];
+
+		for (int i = 0; i < transform.childCount; i++) {
+		
+			sounds[i] = transform.GetChild (i).GetComponent<Sound>();
+		}
+
+		foreach (Sound s in sounds) {
+
+			s.source = gameObject.AddComponent<AudioSource> ();
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+
+		}
+	}
+
+	void Start(){
+
+		Play ("MainTheme");
+	}
+
+	
+		public void Play(string name){
+
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.Play ();
+
+		}
+}
